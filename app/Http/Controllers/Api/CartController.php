599e2822105998fef5efacab46e96f1b39e22689
Lokusok <lokusokdrobk@gmail.com>
2024-11-21 16:33:32 +0300

    Add swagger

--- app/Http/Controllers/Api/CartController.php
+++ app/Http/Controllers/Api/CartController.php
@@ -5,11 +5,30 @@ namespace App\Http\Controllers\Api;
 use App\Http\Resources\Cart\CartResource;
 use App\Models\Article;
 use App\Models\Cart;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
+/**
+ * @OA\Info(title="Cart", version="1.0.0")
+ */
 class CartController
 {
+    /**
+     * @OA\Get(
+     *     path="/api/cart",
+     *     tags={"Cart"},
+     *     @OA\Response(
+     *       response="200",
+     *       description="Получить содержимое корзины",
+     *       @OA\JsonContent(
+     *         @OA\Property(property="data", type="object",
+     *         @OA\Property(property="id", type="number", example=4242),
+     *         @OA\Property(property="title", type="string", example="Swagger Book"),
+     *         @OA\Property(property="price", type="number", example=1800)
+     *       )
+     *     )
+     *   )
+     * )
+     */
     public function index()
     {
         $cart = Cart::query()->with('article')->get();
@@ -17,6 +36,26 @@ class CartController
         return CartResource::collection($cart);
     }
 
+    /**
+     * @OA\Get(
+     *     path="/api/cart/current",
+     *     tags={"Cart"},
+     *     @OA\Response(
+     *       response="200",
+     *       description="Получить статистику лежащего в корзине",
+     *       @OA\JsonContent(
+     *         @OA\Property(property="articles", type="array", @OA\Items(
+     *          @OA\Property(property="id", type="number", example=4242),
+     *          @OA\Property(property="title", type="string", example="Swagger Book"),
+     *          @OA\Property(property="price", type="number", example=1800)
+     *     )
+     *   ),
+     *         @OA\Property(property="total_sum", type="number", example="500"),
+     *       )
+     *     )
+     *   )
+     * )
+     */
     public function cart()
     {
         $result = DB::select('SELECT articles.id, articles.title, articles.price, COUNT(*) as count FROM articles
@@ -31,6 +70,31 @@ class CartController
         ]);
     }
 
+    /**
+     * @OA\Delete(
+     *     path="/api/cart/{itemId}",
+     *     tags={"Cart"},
+     *     @OA\Parameter(
+     *       name="itemId",
+     *       in="path",
+     *       required=true,
+     *       description="ID товара",
+     *       @OA\Schema(type="string")
+     *     ),
+     *     @OA\Response(
+     *       response="200",
+     *       description="Получить статистику лежащего в корзине",
+     *       @OA\JsonContent(
+     *         @OA\Property(
+     *           property="status",
+     *           type="string",
+     *           example="Success",
+     *         )
+     *       )
+     *     )
+     *   )
+     * )
+     */
     public function destroy(string $id)
     {
         $article = Article::findOrFail($id);
